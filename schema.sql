3274732c39c65d78c9a6e0d9374b671c1012c840
ndyanguyen <126118569+ndyanguyen@users.noreply.github.com>
2023-05-04 19:01:05 -0700

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -59,22 +59,23 @@ CREATE TABLE "titles" (
      )
 );
 
+-- fk for foreign keys
 SELECT * FROM titles LIMIT 10;
 
-ALTER TABLE "dept_emp" ADD CONSTRAINT "dept_emp_emp_no" FOREIGN KEY("emp_no")
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
 
-ALTER TABLE "dept_emp" ADD CONSTRAINT "dept_emp_dept_no" FOREIGN KEY("dept_no")
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
 REFERENCES "departments" ("dept_no");
 
-ALTER TABLE "dept_manager" ADD CONSTRAINT "dept_manager_dept_no" FOREIGN KEY("dept_no")
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
 REFERENCES "departments" ("dept_no");
 
-ALTER TABLE "dept_manager" ADD CONSTRAINT "dept_manager_emp_no" FOREIGN KEY("emp_no")
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
 
-ALTER TABLE "employees" ADD CONSTRAINT "employees_emp_title_id" FOREIGN KEY("emp_title_id")
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
 REFERENCES "titles" ("title_id");
 
-ALTER TABLE "salaries" ADD CONSTRAINT "salaries_emp_no" FOREIGN KEY("emp_no")
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
